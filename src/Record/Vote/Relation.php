78b4a3e2473eba2e96f5ee05cd36439a9f814938
Judzhin Miles <info@msbios.com>
2018-02-12 19:38:50 +0200

    Lazy Composer Commiter

--- src/Record/Vote/Relation.php
+++ src/Record/Vote/Relation.php
@@ -3,11 +3,11 @@
  * @access protected
  * @author Judzhin Miles <info[woof-woof]msbios.com>
  */
-namespace MSBios\Voting\Resource\Record\Poll;
+namespace MSBios\Voting\Resource\Record\Vote;
 
 /**
  * Class Relation
- * @package MSBios\Voting\Resource\Record\Poll
+ * @package MSBios\Voting\Resource\Record\Vote
  */
 class Relation
 {
--- src/Record/Vote/RelationInterface.php
+++ src/Record/Vote/RelationInterface.php
@@ -3,13 +3,13 @@
  * @access protected
  * @author Judzhin Miles <info[woof-woof]msbios.com>
  */
-namespace MSBios\Voting\Resource\Record\Poll;
+namespace MSBios\Voting\Resource\Record\Vote;
 
 use MSBios\Voting\Resource\Record\RelationInterface as DefaultRelationInterface;
 
 /**
  * Interface RelationInterface
- * @package MSBios\Voting\Resource\Record\Poll
+ * @package MSBios\Voting\Resource\Record\Vote
  */
 interface RelationInterface extends DefaultRelationInterface
 {
